631ed2c4d92201539ddb304513c28d1f6378aadf
skipperr254 <skipperr254@gmail.com>
2024-11-18 21:57:52 +0300

    Added the caret and it's functionality

--- js/script.js
+++ js/script.js
@@ -108,12 +108,77 @@ const showWords = () => {
     document.getElementById("words").innerHTML = ""
 
     if (testMode == "words" || testMode == "custom") {
-
+        for (let i = 0; i < wordsList.length; i++) {
+            let w = "<div class='word'>"
+            for (let j = 0; j < wordsList[i].length; j++) {
+                w += `<span>${wordsList[i].charAt(j)}</span>`
+            }
+            w += "</div>"
+            document.getElementById("words").innerHTML += w
+        }
     } else if (testMode == "time") {
-        
+        for (let i = 0; i < wordsList.length; i++) {
+            let w = "<div class='word'>"
+            for (let j = 0; j < wordsList[i].length; j++) {
+                w += `<span>${wordsList[i].charAt(j)}</span>`
+            }
+            w += "</div>"
+            document.getElementById("words").innerHTML += w
+        }
+    }
+    updateActiveElement()
+    updateCaretPosition()
+}
+
+const updateActiveElement = () => {
+    document.querySelectorAll("#words .word").forEach(element => { element.classList.remove("active") })
+    let wordDiv = document.querySelectorAll("#words .word")[currentWordIndex]
+    wordDiv.classList.add("active")
+    wordDiv.classList.remove("error")
+}
+
+const updateCaretPosition = () => {
+    let caret = document.querySelector("#caret")
+    let inputLength = currentInput.length
+    let currentLetterIndex = inputLength - 1
+
+    if (currentLetterIndex < 0) {
+        currentLetterIndex = 0
+    }
+
+    let currentLetterSpan = document.querySelectorAll("#words .word.active span")[currentLetterIndex]
+    let currentLetterPosition = currentLetterSpan.getBoundingClientRect()
+    let letterHeight = currentLetterPosition.height
+
+    /* if we have not typed anything yet */
+    if (inputLength == 0) {
+        caret.style.top = currentLetterPosition.top - letterHeight / 4
+        caret.style.left = currentLetterPosition.left - caret.clientWidth / 2
+        console.log("Input length 0")
+    } else {
+        caret.style.top = `${currentLetterPosition.top - letterHeight / 4}px`
+        caret.style.left = `${currentLetterPosition.left + currentLetterSpan.clientWidth - caret.clientWidth / 2}px`
+        console.log(currentLetterPosition.left + currentLetterSpan.clientWidth - caret.clientWidth / 2)
     }
 }
 
 initializeWords()
 
-console.log(wordsList)
+document.querySelector("#wordsInput").addEventListener("keypress", (event) => {
+    event.preventDefault()
+}) 
+
+document.addEventListener("keypress", (event) => {
+    const wordsInput = document.getElementById("wordsInput")
+   
+    // if (!wordsInput.matches(":focus")) return
+
+    if (event.code == "Enter") return
+
+    if (event.code == "Space") return
+
+    currentInput += event.key
+
+    updateCaretPosition()
+    console.log(currentInput)
+})
